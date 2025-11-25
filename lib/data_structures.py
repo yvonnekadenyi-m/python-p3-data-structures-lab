4c7e79da5487a73799c7fde64f4a26b79983d6f7
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2025-11-25 22:09:22 +0300

    added to datastructures.py

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -14,25 +14,3 @@ spicy_foods = [
         "cuisine": "Sichuan",
         "heat_level": 6,
     },
-]
-
-def get_names(spicy_foods):
-    pass
-
-def get_spiciest_foods(spicy_foods):
-    pass
-
-def print_spicy_foods(spicy_foods):
-    pass
-
-def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
-
-def print_spiciest_foods(spicy_foods):
-    pass
-
-def get_average_heat_level(spicy_foods):
-    pass
-
-def create_spicy_food(spicy_foods, spicy_food):
-    pass
